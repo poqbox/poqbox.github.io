b4d48399eaa56861648bfb8ba7b85016c05341df
poq <120293384+poqbox@users.noreply.github.com>
2025-07-19 12:49:11 -0400

    Redesigned the homepage's nav items

--- src/components/HomepageNavItem.jsx
+++ src/components/HomepageNavItem.jsx
@@ -1,4 +1,17 @@
-export default function HomepageNavItem({title}) {
+import { useState, useEffect } from 'react'
+import TypedText from './TypedText'
+
+export default function HomepageNavItem({title, begin_animation=true, setNextAnimationState=null}) {
+  const [Style, setStyle] = useState({height: 0, padding: 0}) // hide the component until the animation begins
+
+
+  // initial effect
+  useEffect(() => {
+    if (begin_animation)
+      setStyle({})  // unhide the component
+  }, [begin_animation])
+
+
   // event handlers
   function onMouseDownHandler(e) {
     e.currentTarget.classList.add("cursor-grabbing")
@@ -18,8 +31,17 @@ export default function HomepageNavItem({title}) {
         onMouseDown={onMouseDownHandler}
         onMouseUp={onMouseUpHandler}
         onMouseLeave={onMouseLeaveHandler}
+        style={Style}
       >
-        <b>{title}</b>
+        <TypedText
+          text={title}
+          style={{fontWeight: "bold"}}
+          base_speed={60}
+          max_text_cursor_blinks={0}
+          custom_ending_duration={60}
+          begin_animation={begin_animation}
+          setNextAnimationState={setNextAnimationState}
+        />
       </a>
     </>
   )
