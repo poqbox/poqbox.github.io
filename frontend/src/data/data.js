59f415d9d9626aa04d85846782286c4bba8c6c97
poq <120293384+poqbox@users.noreply.github.com>
2025-07-21 17:15:43 -0400

    W...I...P
    updated text

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -1,7 +1,7 @@
 export default {
   "homepage-navbar": [
-    {title: "Projects"},
-    {title: "Websites"},
-    {title: "About"}
+    {title: "Work..."},
+    {title: "  In"},
+    {title: "Progress..."}
   ]
 }
