ac1fb5d5c4202287aef366d4daffce379caa5917
poq <120293384+poqbox@users.noreply.github.com>
2025-07-30 13:13:04 -0400

    Refactored UnderConstructionPage to use Content's style prop

--- frontend/src/components/UnderConstructionPage.jsx
+++ frontend/src/components/UnderConstructionPage.jsx
@@ -3,37 +3,36 @@ import Content from "./Content"
 
 export default function UnderConstructionPage({ContentPage, setContentPage}) {
   return (
-    <Content show={ContentPage} setCurrentPage={setContentPage}>
+    <Content
+      style={{backgroundImage: "linear-gradient( #ffcc55, #ff8888 96%, #ff6f44 96% )"}}
+      show={ContentPage}
+      setCurrentPage={setContentPage}
+    >
       <div style={{
-        height: "100%",
-        backgroundImage: "linear-gradient( #ffcc55, #ff8888 96%, #ff6f44 96% )",
-      }}>
-        <div style={{
-          color: "white",
-          fontFamily: "Candara",
-          fontSize: "20px",
-          fontWeight: "bold",
-          width: "fit-content",
-          position: "relative",
-          top: "30%",
-          margin: "auto",
-          textAlign: "center"
-        }}
-        >
-          <div style={{fontSize: "64px", marginBottom: "16px"}}>{"< under-construction >"}</div>
-          <div>In the meantime,</div>
-          <div>you can find me here:</div>
-          <div style={{marginTop: "8px", display: "flex", justifyContent: "center"}}>
-            <div className='footer-icon-container' style={{margin: "0 12px"}}>
-              <a href='https://www.linkedin.com/in/liukevin223/' target='_blank'>
-                <img className='footer-icon linkedin' src='icons/linkedin.svg' />
-              </a>
-            </div>
-            <div className='footer-icon-container' style={{margin: "0 12px"}}>
-              <a href='https://github.com/poqbox' target='_blank'>
-                <img className='footer-icon github' src='icons/github.svg' />
-              </a>
-            </div>
+        color: "white",
+        fontFamily: "Candara",
+        fontSize: "20px",
+        fontWeight: "bold",
+        width: "fit-content",
+        position: "relative",
+        top: "30%",
+        margin: "auto",
+        textAlign: "center"
+      }}
+      >
+        <div style={{fontSize: "64px", marginBottom: "16px"}}>{"< under-construction >"}</div>
+        <div>In the meantime,</div>
+        <div>you can find me here:</div>
+        <div style={{marginTop: "8px", display: "flex", justifyContent: "center"}}>
+          <div className='footer-icon-container' style={{margin: "0 12px"}}>
+            <a href='https://www.linkedin.com/in/liukevin223/' target='_blank'>
+              <img className='footer-icon linkedin' src='icons/linkedin.svg' />
+            </a>
+          </div>
+          <div className='footer-icon-container' style={{margin: "0 12px"}}>
+            <a href='https://github.com/poqbox' target='_blank'>
+              <img className='footer-icon github' src='icons/github.svg' />
+            </a>
           </div>
         </div>
       </div>
